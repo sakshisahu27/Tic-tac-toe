591d38225b55715ed918f641f34b8af5079c3532
sakshisahu27 <sakshisahu6999@gmail.com>
2024-06-18 16:53:52 +0530

    tic tac toe game

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TicTacToe from './Componenets/TicTacToe/TicTacToe';
+
+function App() {
+  return (
+    <div >
+      <TicTacToe />
+    </div>
+  );
+}
+
+export default App;
